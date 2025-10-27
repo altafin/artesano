e6cfb8106135a3a4b6b55a314a1f22917e960dd9
altafin <marbr72@hotmail.com>
2025-10-26 22:46:50 -0300

    Reapply "Criado o modelo para categoria e os arquivos do Filament para ele."
    This reverts commit bb73b879ed98d4fea29e6981ba1a2d7f1027f67e.

---
+++ app/Filament/Resources/Categories/Pages/EditCategories.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Filament\Resources\Categories\Pages;
+
+use App\Filament\Resources\Categories\CategoriesResource;
+use Filament\Actions\DeleteAction;
+use Filament\Actions\ViewAction;
+use Filament\Resources\Pages\EditRecord;
+
+class EditCategories extends EditRecord
+{
+    protected static string $resource = CategoriesResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            ViewAction::make(),
+            DeleteAction::make(),
+        ];
+    }
+}
